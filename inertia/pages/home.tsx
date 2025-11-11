a00fb7665fb75204dce6aeda6e1a0787cd08f617
Clément Mistral <55872681+clement-mistral@users.noreply.github.com>
2025-11-11 20:54:53 +0100

    Added version

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,4 +1,5 @@
 import { Head, router } from '@inertiajs/react'
+import { version } from '~/app/version';
 
 type User = {
   firstname: string;
@@ -18,6 +19,7 @@ export default function HomePage({ user }: HomePageProps) {
       <Head title="Homepage" />
       <main className="h-screen flex flex-col items-center justify-center gap-10">
         <h1 className="font-semibold text-4xl text-white">Bonjour {user.firstname} {user.lastname}</h1>
+        <p className="text-white font-bold">v{version}</p>
         <button type="button" className="bg-white p-2 rounded" onClick={() => router.delete('/logout')}>Se déconnecter</button>
       </main>
     </>
